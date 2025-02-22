7e69d31043140a8fbe2df1f00d9e6e23aaddc984
DucChung2123 <chungcgcg@gmail.com>
2025-02-23 00:17:06 +0700

    Feat: Add API v1 to test model inference

---
+++ src/inference/predict.py
@@ -0,0 +1,72 @@
+import torch
+from transformers import AutoTokenizer
+from src.training.model import ToxicClassifier
+import yaml
+
+class ToxicClassifierAPI:
+    
+    def __init__(self, config_path="configs/config.yaml"):
+        with open(config_path, "r") as f:
+            self.config = yaml.safe_load(f)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = ToxicClassifier(
+            self.config["model"]["name"], num_labels=self.config["model"]["num_labels"]
+        )
+        self.model.load_state_dict(torch.load(
+            self.config["training"]["save_model_path"] + ".pth",
+            map_location=self.device
+        ))
+        self.model.to(self.device)
+        self.model.eval()
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.config["model"]["name"])
+        
+    def predict(self, text: str):
+        encoding = self.tokenizer(
+            text,
+            padding="max_length",
+            truncation=True,
+            max_length=self.config["training"]["max_length"],
+            return_tensors="pt"
+        )
+        input_ids = encoding["input_ids"].to(self.device)
+        attention_mask = encoding["attention_mask"].to(self.device)
+
+        with torch.no_grad():
+            outputs = self.model(input_ids, attention_mask)
+            probs = torch.nn.functional.softmax(outputs, dim=1)
+            predicted_class = torch.argmax(probs, dim=1).item()
+
+        return {
+            "text": text,
+            "label": "toxic" if predicted_class == 1 else "normal",
+            "confidence": probs[0, predicted_class].item()
+        }
+    
+    def batch_predict(self, texts: list[str]):
+        encodings = self.tokenizer(
+            texts,
+            padding="max_length",
+            truncation=True,
+            max_length=self.config["training"]["max_length"],
+            return_tensors="pt",
+            return_attention_mask=True
+        )
+        input_ids = encodings["input_ids"].to(self.device)
+        attention_mask = encodings["attention_mask"].to(self.device)
+
+        with torch.no_grad():
+            outputs = self.model(input_ids, attention_mask)
+            probs = torch.nn.functional.softmax(outputs, dim=1)
+            predicted_classes = torch.argmax(probs, dim=1)
+
+        results = []
+        for text, predicted_class, prob in zip(texts, predicted_classes, probs):
+            results.append({
+                "text": text,
+                "label": "toxic" if predicted_class == 1 else "normal",
+                "confidence": prob[predicted_class].item()
+            })
+
+        return results
--- src/training/model.py
+++ src/training/model.py
@@ -4,11 +4,11 @@ from transformers import AutoModel
 
 class ToxicClassifier(nn.Module):
     
-    def __init__(self, model_name: str, num_lables: int = 2, cache_dir: str = None):
+    def __init__(self, model_name: str, num_labels: int = 2, cache_dir: str = None):
         super(ToxicClassifier, self).__init__()
         self.model = AutoModel.from_pretrained(model_name, cache_dir=cache_dir)
         self.dropout = nn.Dropout(0.3)
-        self.classifier = nn.Linear(self.model.config.hidden_size, num_lables)
+        self.classifier = nn.Linear(self.model.config.hidden_size, num_labels)
     
     def forward(self, input_ids, attention_mask):
         outputs = self.model(
